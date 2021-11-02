dbd5614e88704aad7188e2a1a6120ec2bed82404
Maaruni <maarunip.2020@scis.smu.edu.sg>
2021-11-02 22:06:38 +0800

    final

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import streamlit as st
 from multiapp import MultiApp
 import home_app_1
 import face_blur_app_2  # , methodology_app, prototype_app
-
+import mobile_prediction
 app = MultiApp()
 
 # Navigation
 app.add_app("Actual Idiots", home_app_1.app)
 app.add_app("Face Blurring", face_blur_app_2.app)
+app.add_app("Model Prediction", mobile_prediction.app)
 # app.add_app("Methodology", methodology_app.app)
 # app.add_app("Prototype", prototype_app.app)
 st.set_page_config(layout='wide')
