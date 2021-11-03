df7a5d01a24996a61b23cedcefb325809ebf3be6
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2021-11-03 15:41:33 +0800

    font size

--- app4_predict_mobilenet.py
+++ app4_predict_mobilenet.py
@@ -206,9 +206,9 @@ def app():
                         "talking on the phone - left", "operating the radio", 
                         "drinking", "reaching behind", 
                         "hair and makeup", " talking to passenger"]
-            st.write(f"It's a '{categories[first_img_category]}' picture.", '\n' )
+            st.header(f"It's a '{categories[first_img_category]}' picture.", '\n' )
             
-            st.write('**Probability: **',probability,'%')
+            st.header('**Probability: **',probability,'%')
                 
                 # st.sidebar.audio(audio_bytes)
                                 
